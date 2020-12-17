bb53089180550270246be55edf8dff4dbc2201f0
Luke Schweickart <lukeschweickart@gmail.com>
2020-12-16 23:01:36 -0600

    Initial commit

---
+++ src/main/java/org/esmp/test/Test.java
@@ -0,0 +1,29 @@
+package org.esmp.test;
+
+import java.io.File;
+import java.util.logging.Logger;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Test extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
